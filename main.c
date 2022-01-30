db4fa0f329d885069c1254831db89e9ed9657ec2
Christian Reinbacher <reini1305@gmail.com>
2022-01-30 19:39:01 +0100

    apples can also spawn next to each other

--- main.c
+++ main.c
@@ -13,7 +13,9 @@
 #define UP    2
 #define DOWN  3
 #define UPDATE_SNAKE 8
+#define OFFSET 128
 #define APPLE 144
+#define WALL  143
 
 const int8_t delta_x[4] = {1, -1, 0, 0};
 const int8_t delta_y[4] = {0, 0, -1, 1};
@@ -40,7 +42,7 @@ void update_score(void) {
 
 void update_tail(void) {
     uint8_t tile;
-    set_vram_byte(get_bkg_xy_addr(tail_x, tail_y), 128);
+    set_vram_byte(get_bkg_xy_addr(tail_x, tail_y), OFFSET);
     // check which tail to draw
     tile = get_bkg_tile_xy(tail_x+1, tail_y);
     if (tile == 130 || tile == 131 || tile == 133){
@@ -120,15 +122,15 @@ void update_scroll(void) {
 }
 
 void spawn_apple(void) {
-    uint8_t x = min(max(1, (arand() + 128u) / 8), 30);
-    uint8_t y = min(max(2, (arand() + 128u) / 8), 30);
-    while ((get_bkg_tile_xy(x, y) != 128) || 
-           (get_bkg_tile_xy(x+1, y) != 128) ||
-           (get_bkg_tile_xy(x, y+1) != 128) ||
-           (get_bkg_tile_xy(x-1, y) != 128) ||
-           (get_bkg_tile_xy(x, y-1) != 128)) {
-        x = min(max(1, (arand() + 128u) / 8), 30);
-        y = min(max(2, (arand() + 128u) / 8), 30);
+    uint8_t x = min(max(1, (arand() + OFFSET) / 8), 30);
+    uint8_t y = min(max(2, (arand() + OFFSET) / 8), 30);
+    while ((get_bkg_tile_xy(x, y) != OFFSET) || 
+           (get_bkg_tile_xy(x+1, y) == WALL) ||
+           (get_bkg_tile_xy(x, y+1) == WALL) ||
+           (get_bkg_tile_xy(x-1, y) == WALL) ||
+           (get_bkg_tile_xy(x, y-1) == WALL)) {
+        x = min(max(1, (arand() + OFFSET) / 8), 30);
+        y = min(max(2, (arand() + OFFSET) / 8), 30);
     }
     VBK_REG = 1;
     set_vram_byte(get_bkg_xy_addr(x, y), 2);
@@ -145,7 +147,7 @@ uint8_t move_snake(void) {
 
     // check if we hit something
     tile = get_bkg_tile_xy(head_x, head_y);
-    if (tile > 128 && tile < APPLE) {
+    if (tile > OFFSET && tile < APPLE) {
         // we hit something, game over
         return 1;
     } 
@@ -223,7 +225,7 @@ void reset_game(uint8_t show_title_screen) {
     score = 0;
     enable_move = 0;
     for(i=0; i<20-num_apples; i++){
-        set_vram_byte(get_bkg_xy_addr(i,0), 128);
+        set_vram_byte(get_bkg_xy_addr(i,0), OFFSET);
     }
     VBK_REG = 1;
     set_bkg_tiles(0, 0, 32, 32, background_mapPLN1);
@@ -271,7 +273,7 @@ void main(void)
     uint8_t i;
     HIDE_BKG;
     printf(" "); // to initialize tile map
-    set_bkg_data(128, 18, tile_map);
+    set_bkg_data(OFFSET, 18, tile_map);
     set_bkg_palette(0, 1, &tile_map_palettes[0]);
     set_bkg_palette(1, 1, &tile_map_palettes[4]);
     set_bkg_palette(2, 1, &tile_map_palettes[8]);
